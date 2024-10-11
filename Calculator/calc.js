1d599d10343e87646f601e4154890e33f6c0880a
Chinomso <chinomsoaugustine073@gmail.com>
2024-10-11 08:26:57 -0700

    some bugs fixed

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -18,12 +18,40 @@ function calculator() {
 //function to handle basic operations
 function basicOperation(value) {
     let toInt = 0;
-    let answer = 0; 
+    let answer = 0;
 
     switch (value) {
         case '=':
             try {
-                result = eval(result.trim()).toString();
+                //check for sinh, cosh, and tanh
+                if (result.includes('sinh(') || result.includes('cosh(') || result.includes('tanh')) {
+
+                    //capture the function name
+                    const trigOperator = result.substring(0, result.includes('('));
+
+                    //capture the value b\w parenthesis using substring which extracts parts of strings b/w start and end
+                    //first: find index of opening parenthesis
+                    //2nd: find index of closing p
+                    const innerVal = result.substring(result.indexOf('(') + 1, result.indexOf(')'));
+
+                    //convert value to float
+                    toInt = parseFloat(innerVal);
+
+                    //calculate based on input
+                    if (!isNaN(toInt)) {
+                        answer = (trigOperator === 'sinh' ? Math.sinh(toInt) :
+                            (trigOperator === 'cosh' ? Math.cosh(toInt) :
+                                Math.tanh(toInt))).toExponential(6);
+                        result = answer.replace("e+", "e");
+
+                    }
+                    else {
+                        result = "error";
+                    }
+                }
+                else {
+                    result = eval(result.trim()).toString();
+                }
             } catch (e) {
                 result = "Error";
             }
@@ -54,14 +82,17 @@ function basicOperation(value) {
         case 'sinh':
         case 'cosh':
         case 'tanh':
-            toInt = parseFloat(result);
-            answer = (value === 'sinh' ? Math.sinh(toInt) : (value === 'cosh' ? Math.cosh(toInt): Math.tanh(toInt))).toExponential(6);
-            result = answer.replace("e+", "e");
-            break;
 
+            result += value + '()';
+            display.value = result;
+
+            const position = result.length - 1;
+            display.setSelectionRange(position, position);
+            display.focus();
+            break;
         case 'π':
-            let piValue = Math.PI.toFixed(9);
-            result *=piValue;
+            let piValue = Math.PI.toFixed(8);
+            result *= piValue;
             break;
 
         default:
@@ -72,33 +103,37 @@ function basicOperation(value) {
 }
 
 
-function cursorDisplay() {
-    if (display.value.endsWith('|')) {
-        display.value = display.value.slice(0, -1); //removes couser
-    }
-    else {
-        display.value += '|';
-    }
-}
-setInterval(cursorDisplay, 500);
-
 //Listening to use keyboard input
 function keyboardListener() {
 
     document.addEventListener('keydown', function (e) {
         const keyValue = e.key;
 
-        //keys to ignore: Do nothing when those keys are pressed
-        if (['Control', 'Shift', 'Alt'].includes(keyValue)) {
-            return;
-        }
-
+        const allowedCommands = ['ArrowLeft', 'ArrowRight', 'Backspace', 'Delete', 'Enter', 'Escape'];
         //Since enter and = are identical and Escape and Clear are identical, store them in different variables and chose them randomly
         const keyMap = {
             'Enter': '=',
             'Backspace': 'AC',
             'Escape': 'AC'
         };
+
+
+        //preventing invalid keys
+        if (!allowedCommands.includes(keyValue) && isNaN(Number(keyValue))&& keyValue !== '(' && keyValue !== ')') {
+            e.preventDefault();
+            return;
+        }
+        //Ignoring control, shift, and alt
+        if (['Control', 'Shift', 'Alt'].includes(keyValue)) {
+            return;
+        };
+
+        //handling parenthesis
+        if (keyValue === '(' || keyValue === ')') {
+            basicOperation(keyValue);
+            return;
+
+        }
 
         //Handling ctrl = for selecting = sign and ctrl 8 for * 
         if (e.ctrlKey && keyValue === '=') {
@@ -111,13 +146,13 @@ function keyboardListener() {
         }
 
         //Choose identical keys randomly 
-        const value = keyMap[keyValue] || keyValue;
+        const command = keyMap[keyValue] || keyValue;
 
-        const keysAllowed = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '=', 'Enter', 'Escape', 'Clear'];
+        const keysAllowed = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '=', 'Clear'];
 
         //Checking if user key is on the list
-        if (keysAllowed.includes(keyValue) || keyValue in keyMap) {
-            basicOperation(value);
+        if (keysAllowed.includes(command)) {
+            basicOperation(command);
         }
         else {
             display.textContent = "Wrong Key";
