9414d547db45934551f361dc5db0814aec1f0d4e
Chinomso <chinomsoaugustine073@gmail.com>
2024-11-11 13:44:26 -0800

    sin and relatives done

--- Calculator/calc.js
+++ Calculator/calc.js
@@ -1,17 +1,15 @@
-const display = document.querySelector(".display");
+const display = document.querySelector('.display');
 let result = "";
 let waitingForOperand = false;
 
 function calculator() {
   //Using eventlistener to update when button is clicked
-  document.querySelector(".calculator").addEventListener("click", function (e) {
-    if (e.target.tagName === "BUTTON") {
+  document.querySelector('.calculator').addEventListener('click', function (e) {
+    if (e.target.tagName === 'BUTTON') {
       const buttonValue = e.target.textContent;
       operation(buttonValue);
     }
   });
-  //Initializing keyboard listening
-  // keyboardListener();
 }
 
 //function to handle basic operations
@@ -20,71 +18,62 @@ function operation(value) {
   let answer = 0;
 
   switch (value) {
-    case "=":
-      {
-        try {
-          if (
-            result.includes("sinh") ||
-            result.includes("cosh") ||
-            result.includes("tanh") ||
-            result.includes("sin") ||
-            result.includes("cos") ||
-            result.includes("tan")
-          ) {
-            // Matching number after trigonometric function
-            const match = result.match(
-              /(sinh|cosh|tanh|sin|cos|tan)\(([^)]+)\)/
-            );
-            if (match) {
-              // Extracting the trigonometric function and the number to calculate
-              const extractTrigFun = match[1];
-              const numToCalc = parseFloat(match[2].trim());
+    case '=':
+      try {
+        if (result.includes('sinh') || result.includes('cosh') || result.includes('tanh') || result.includes('sin') || result.includes('cos') || result.includes('tan')) {
+          //matching num after sinh
+          const match = result.match(/(sinh|cosh|tanh|sin|cos|tan)\(([^)]+)\)/) || result.match(/(sinh|cosh|tanh|sin|cos|tan)(\d+)/);
+          if (match) {
+            //extracting trig functions 
+            const extractTrigFun = match[1];
+            const numToCalc = parseFloat(match[2].trim());
 
-              // Checking if numToCalc is a valid number
-              if (!isNaN(numToCalc)) {
-                switch (extractTrigFun) {
-                  case "sinh":
-                    answer = Math.sinh(numToCalc).toExponential(6);
-                    break;
-                  case "cosh":
-                    answer = Math.cosh(numToCalc).toExponential(6);
-                    break;
-                  case "tanh":
-                    answer = Math.tanh(numToCalc).toExponential(6);
-                    break;
-                  case "sin":
-                    answer = Math.sin(numToCalc).toExponential(6);
-                    break;
-                  case "cos":
-                    answer = Math.cos(numToCalc).toExponential(6);
-                    break;
-                  case "tan":
-                    answer = Math.tan(numToCalc).toExponential(6);
-                    break;
-                }
-
-                // Replace "e+" to display exponential in a simpler format
-                result = answer.toString().replace("e+", "e");
-              } 
-              
-              else {
-                result = "Not a number"; // Ensure the value after trig function is valid
+            //checking if numToCal is an actual number
+            if (!isNaN(numToCalc)) {
+              if (extractTrigFun === 'sinh') {
+                answer = Math.sinh(numToCalc);
               }
-            } else {
-              result = "Not a valid trig function format";
+              else if (extractTrigFun === 'cosh') {
+                answer = Math.cosh(numToCalc);
+              }
+              else if (extractTrigFun === 'tanh') {
+                answer = Math.tanh(numToCalc);
+              }
+              else if(extractTrigFun === 'sin'){
+                answer = Math.sin(numToCalc);
+              }
+              else if(extractTrigFun === 'cos'){
+                answer = Math.cos(numToCalc);
+              }
+              else if(extractTrigFun === 'tan'){
+                answer = Math.tan(numToCalc);
+              }
+              result = answer.toString().replace("e+", "e");
+            }
+
+            else {
+              result = "Not a number";
             }
           }
-        } catch (e) {
-          result = "Calculation error";
+          else {
+            result = "Not Trig Fun";
+          }
         }
+        else {
+
+          result = eval(result.trim()).toString();
+        }
+      }
+      catch (e) {
+        result = "Unexpected Calculation";
       }
       break;
 
-    case "AC":
-      result = "";
+    case 'AC':
+      result = '';
       break;
 
-    case "+/-":
+    case '+/-':
       if (result.startsWith("-")) {
         // Removing negative sign if present
         result = result.substring(1);
@@ -94,64 +83,70 @@ function operation(value) {
       }
       break;
 
-    case "%":
+    case '%':
       result = (parseFloat(result) / 100).toString();
       break;
 
-    case "Rand":
+    case 'Rand':
       result = Math.random().toFixed(8);
       break;
 
-    case "sinh":
+    case 'sinh':
       result = "sinh";
       waitingForOperand = true;
       break;
 
-    case "cosh":
+    case 'cosh':
       result = "cosh";
       waitingForOperand = true;
       break;
 
-    case "tanh":
+    case 'tanh':
       result = "tanh";
       waitingForOperand = true;
       break;
 
-    case "sin":
+      case 'sin':
       result = "sin";
       waitingForOperand = true;
       break;
 
-    case "cos":
+    case 'cos':
       result = "cos";
       waitingForOperand = true;
       break;
 
-    case "tan":
+    case 'tan':
       result = "tan";
       waitingForOperand = true;
       break;
 
-    case "π":
+    case 'π':
       let piValue = Math.PI.toFixed(8);
       toInt = parseFloat(result);
       if (!isNaN(toInt)) {
-        result = toInt * piValue;
+        result = (toInt * piValue)
+        // Replace "e+" to display exponential in a simpler format
+        result = answer.toString().replace("e+", "e");
+
       } else {
         result = piValue.toString();
       }
       break;
 
-    case "x!":
+    case 'x!':
       try {
         //converting current num for factorial
         const curNum = parseFloat(result.trim(), 10);
 
         //Ensure input is not negative
-        if (isNaN(curNum) || curNum < 0 || !Number.isInteger(curNum)) {
-          result = "Invalid number";
+        if (isNaN(curNum) ||  !Number.isInteger(curNum)) {
+          result = "Choose number first";
+        }
+        else if(curNum < 0 ){
+          result = "Cannot be negative"
         } else {
-          result = factorialRecursive(curNum).toExponential(6);
+          result = factorialRecursive(curNum);
         }
       } catch (e) {
         result = "Unexpected calculator Error";
@@ -164,11 +159,12 @@ function operation(value) {
         result += value;
         waitingForOperand = false; // Reset the flag as we have the operand now
       } else {
-        result += value; // Handles other values by appending to the result
+        result += value;  // Handles other values by appending to the result
       }
       break;
   }
-  display.value = result; // Updates the display with the current result
+  display.value = result;  // Updates the display with the current result
+
 }
 
 function convertAngle(input, format) {
@@ -184,61 +180,63 @@ function factorialRecursive(n) {
     return 1;
   }
 
+
   return n * factorialRecursive(n - 1);
 }
+
+
+
 
 /*
 //Listening to use keyboard input
 function keyboardListener() {
+
  
-    document.addEventListener('keydown', function (e) {
-        const keyValue = e.key;
+   document.addEventListener('keydown', function (e) {
+       const keyValue = e.key;
+
  
-        const allowedCommands = ['ArrowLeft', 'ArrowRight', 'Backspace', 'Delete', 'Enter', 'Escape'];
-        //Since enter and = are identical and Escape and Clear are identical, store them in different variables and chose them randomly
-        const keyMap = {
-            'Enter': '=',
-            'Backspace': 'AC',
-            'Escape': 'AC'
-        };
- 
- 
-        //preventing invalid keys
-        if (!allowedCommands.includes(keyValue) && isNaN(Number(keyValue))&& keyValue !== '(' && keyValue !== ')') {
-            e.preventDefault();
-            return;
-        }
-       
- // Handling special keys
- if (keyMap.hasOwnProperty(keyValue)) {
-    basicOperation(keyMap[keyValue]);
-    return;
-}
-        //handling parenthesis
-        if (keyValue === '(' || keyValue === ')') {
-            basicOperation(keyValue);
-            return;
- 
-        }
- 
-        //Handling ctrl = for selecting = sign and ctrl 8 for * 
-      
-        if (e.shiftKey && keyValue === '*') {
-            basicOperation('*');
-            return;
-        }
+       const allowedCommands = ['ArrowLeft', 'ArrowRight', 'Backspace', 'Delete', 'Enter', 'Escape'];
+       //Since enter and = are identical and Escape and Clear are identical, store them in different variables and chose them randomly
+       const keyMap = {
+           'Enter': '=',
+           'Backspace': 'AC',
+           'Escape': 'AC'
+       };
+
+
  
  
-        const keysAllowed = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '=', 'Clear'];
+       //preventing invalid keys
+       if (!allowedCommands.includes(keyValue) && isNaN(Number(keyValue))&& keyValue !== '(' && keyValue !== ')') {
+           e.preventDefault();
+@@ -181,19 +218,19 @@ function keyboardListener() {
+       if (keyValue === '(' || keyValue === ')') {
+           basicOperation(keyValue);
+           return;
+
  
-        //Checking if user key is on the list
-        if (keysAllowed.includes(keyValue)) {
-            basicOperation(keyValue);
-        }
-        else {
-            display.textContent = "Wrong Key";
-        }
-    });
+       }
+
+ 
+       //Handling ctrl = for selecting = sign and ctrl 8 for * 
+     
+       if (e.shiftKey && keyValue === '*') {
+           basicOperation('*');
+           return;
+       }
+
+
+ 
+ 
+       const keysAllowed = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '-', '*', '/', '=', 'Clear'];
+
+ 
+       //Checking if user key is on the list
+       if (keysAllowed.includes(keyValue)) {
+           basicOperation(keyValue);
+@@ -204,4 +241,4 @@ function keyboardListener() {
+   });
 }
 */
 calculator();
