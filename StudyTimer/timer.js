3076fcbd091f92f9e56d9303ae6a35e9399dcbff
Chino <chinomsoaugustine073@gmail.com>
2025-04-11 16:11:52 -0700

    Started portfolio

---
+++ StudyTimer/timer.js
@@ -0,0 +1,8 @@
+
+
+//get the values for display purpose 
+let hours = document.querySelector('.Hrs'); 
+let minutes = document.querySelector('.Min'); 
+let seconds = document.querySelector('.Sec'); 
+
+//function to get time
