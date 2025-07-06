d02b2409e0f17d91ea000930daef0b1c97a77b4f
Chino <chinomsoaugustine073@gmail.com>
2025-07-05 22:26:24 -0700

    Deleted

--- StudyTimer/timer.js
+++
@@ -1,8 +0,0 @@
-
-
-//get the values for display purpose 
-let hours = document.querySelector('.Hrs'); 
-let minutes = document.querySelector('.Min'); 
-let seconds = document.querySelector('.Sec'); 
-
-//function to get time
